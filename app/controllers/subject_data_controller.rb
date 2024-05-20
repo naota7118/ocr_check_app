f448f4ef612db3d297c5057f4cbb72f716399338
naota7118 <naota7118@gmail.com>
2024-05-20 21:23:33 +0900

    コントローラのindexメソッドを機能ごとのメソッドに分解

--- app/controllers/subject_data_controller.rb
+++ app/controllers/subject_data_controller.rb
@@ -1,4 +1,46 @@
+require 'google/apis/drive_v3'
+require 'google/api_client/client_secrets'
+require 'csv'
+require 'roo'
+
 class SubjectDataController < ApplicationController
+
+  def convert(drive) #PDFファイルからテキストファイルに変換
+    # Google Driveにファイルをアップロード
+    metadata = Google::Apis::DriveV3::File.new(title: 'My document')
+    metadata = drive.create_file(metadata, upload_source: './tmp/sample.pdf', content_type: 'application/pdf')
+
+    # Googleドキュメント形式に変換
+    converted_file = drive.copy_file(metadata.id, Google::Apis::DriveV3::File.new(mime_type: 'application/vnd.google-apps.document'))
+      
+    # テキストファイルを出力
+    drive.export_file(converted_file.id, 'text/plain', download_dest: './tmp/sample.txt')
+  end
+
+  def get_id_from_text #テキストファイルからIDを取得
+    @pdf_id = []
+    File.open("./tmp/sample.txt", "r") do |f|
+      f.each_line do |l|
+        s = l.chomp.strip
+        if s.include?('CHIBA')
+          @pdf_id.push(s)
+        end
+      end
+    end
+  end
+
+  def get_id_from_excel #エクセルファイルからIDを取得
+    # Excelからデータを取得
+    xlsx = Roo::Excelx.new("./tmp/sample.xlsx")
+    @excel_data = xlsx.parse(headers: true, clean: true)
+
+    #Excelからidだけ取得
+    @excel_id = @excel_data.map do |hash|
+      hash['被験者番号']
+    end
+    # 配列の先頭はidではないので削除
+    @excel_id.shift
+  end
 
   # PDFデータとExcelデータを照合する
   def verify_suject_id(pdf_data, excel_data)
@@ -17,12 +59,18 @@ class SubjectDataController < ApplicationController
     end
   end
 
-  def index  
-    require 'google/apis/drive_v3'
-    require 'google/api_client/client_secrets'
-    require 'csv'
-    require 'roo'
-  
+  def index
+    pass_authentication
+    return if performed?
+    convert(@drive)
+    get_id_from_text
+    get_id_from_excel
+    # PDFデータとExcelデータを照合する
+    verify_suject_id(@pdf_id, @excel_id)
+  end
+
+  private
+  def pass_authentication # Google API認証を通す
     # client_secret.jsonファイルを読み取ってオブジェクトを作成
     client_secrets = Google::APIClient::ClientSecrets.load
     auth_client = client_secrets.to_authorization
@@ -45,45 +93,8 @@ class SubjectDataController < ApplicationController
 
       client_opts = JSON.parse(session[:credentials])
       auth_client = Signet::OAuth2::Client.new(client_opts)
-      drive = Google::Apis::DriveV3::DriveService.new
-      drive.authorization = auth_client
-
-      # ファイルをアップロード
-      metadata = Google::Apis::DriveV3::File.new(title: 'My document')
-      metadata = drive.create_file(metadata, upload_source: './tmp/sample.pdf', content_type: 'application/pdf')
-
-      # Googleドキュメント形式に変換
-      converted_file = drive.copy_file(metadata.id, Google::Apis::DriveV3::File.new(mime_type: 'application/vnd.google-apps.document'))
-      
-      # テキストファイルを出力
-      drive.export_file(converted_file.id, 'text/plain', download_dest: './tmp/sample.txt')
-
-      # テキストファイルから文字を抽出
-      @pdf_id = []
-      File.open("./tmp/sample.txt", "r") do |f|
-        f.each_line do |l|
-          s = l.chomp.strip
-          if s.include?('CHIBA')
-            @pdf_id.push(s)
-          end
-        end
-      end
-
-      # Excelからデータを取得
-      xlsx = Roo::Excelx.new("./tmp/sample.xlsx")
-      @excel_data = xlsx.parse(headers: true, clean: true)
-
-      #Excelからidだけ取得
-      @excel_id = @excel_data.map do |hash|
-        hash['被験者番号']
-      end
-      # 配列の先頭はidではないので削除
-      @excel_id.shift
-
-      # PDFデータとExcelデータを照合する
-      verify_suject_id(@pdf_id, @excel_id)
+      @drive = Google::Apis::DriveV3::DriveService.new
+      @drive.authorization = auth_client
     end
   end
-
 end
-
