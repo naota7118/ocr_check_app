ae54253a3727716f1354194398320091db7da59a
naota7118 <naota7118@gmail.com>
2024-06-02 14:14:00 +0900

    idが不一致のレコードに色を塗る

--- app/javascript/result.js
+++ app/javascript/result.js
@@ -1,1 +1,13 @@
-console.log('hello');
+var result = document.getElementsByClassName('result');
+
+// 不一致のレコードに色を塗る
+function color (){
+  for (let i = 0; i < result.length; i++) {
+    let result_text = result[i].innerText;
+    if (result_text === '一致しません') {
+      result[i].style.backgroundColor = "#FFC0CB";
+    }
+  }
+}
+
+color();
