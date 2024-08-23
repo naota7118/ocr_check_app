fd17d3d28cca3be48e016dc29c3304f5f62f3e88
naota7118 <naota7118@gmail.com>
2024-08-23 19:27:31 +0900

    ログイン後とログアウト後の遷移先を指定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+  private
+
+  def after_sign_in_path_for(resource)
+    moca_data_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end
