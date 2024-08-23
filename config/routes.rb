3837af07f4e894bbb8a8ff5df33a9a512856d105
naota7118 <naota7118@gmail.com>
2024-08-23 16:14:14 +0900

    route.rbの余計な記述を削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :subject_data
   resources :moca_data
-  # get '/', to: 'moca_data#index'
-  get '/result', to: 'subject_data#result'
   get '/moca_result', to: 'moca_data#result'
 end
