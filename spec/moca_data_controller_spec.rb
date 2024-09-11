221ec1eb3792be004dfbbe23e5bebaec56e5012f
naota7118 <naota7118@gmail.com>
2024-09-11 19:31:17 +0900

    Rspecテストのコメントアウトを外す

--- spec/moca_data_controller_spec.rb
+++ spec/moca_data_controller_spec.rb
@@ -2,12 +2,12 @@
 
 require 'rails_helper'
 
-# RSpec.describe MocaDataController, type: :controller do
-#   describe 'GET #index' do
-#     it 'returns a sucessful response' do
-#       get :index
-#       expect(response).to have_http_status(:success)
-#       expect(response).to render_template(:index)
-#     end
-#   end
-# end
+RSpec.describe MocaDataController, type: :controller do
+  describe 'GET #index' do
+    it 'returns a sucessful response' do
+      get :index
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
+    end
+  end
+end
