c9f9d9884dffdc1d43ff2fa949eeb70ad0c5a1b3
naota7118 <naota7118@gmail.com>
2024-09-11 19:56:36 +0900

    Userモデルのテストを修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   
-  user = User.new(id: 1, name: "aaa", email: "aaa@email.com", password: "password")
+  user = User.new(id: 1, email: "aaa@email.com", password: "password")
   
   it "メールアドレスが空だと無効になる" do
     expect(user.email = nil).not_to eq be_valid
