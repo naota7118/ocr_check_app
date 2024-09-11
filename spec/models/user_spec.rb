6c4f1a04307d5266a61cdcd2d5e76395fb179bef
naota7118 <naota7118@gmail.com>
2024-09-11 20:02:18 +0900

    Rubocopエラーを修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
-  user = User.new(id: 1, email: "aaa@email.com", password: "password")
-  
-  it "メールアドレスが空だと無効になる" do
+  user = User.new(id: 1, email: 'aaa@email.com', password: 'password')
+
+  it 'メールアドレスが空だと無効になる' do
     expect(user.email = nil).not_to eq be_valid
   end
 end
