881e8a910de4afe5b40992377c881515c57c3daf
naota7118 <naota7118@gmail.com>
2024-10-16 21:42:13 +0900

    テストを追加(PDFファイルを送る)

--- test/controllers/moca_data_controller_test.rb
+++ test/controllers/moca_data_controller_test.rb
@@ -3,7 +3,14 @@
 require 'test_helper'
 
 class MocaDataControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get moca_data_url
+    assert_response :success
+  end
+  
+  test "should post index" do
+    image = File.open("./test/fixtures/files/sample.pdf", "rb:ASCII-8BIT:UTF-8")
+    post moca_data_url, params: image
+    assert_response :success
+  end
 end
