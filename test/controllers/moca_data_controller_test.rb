508330f8ac4e63ed8ebf51aaca51d516d21bec6b
naota7118 <naota7118@gmail.com>
2024-10-17 13:33:19 +0900

    ファイルアップロードのテストを追加

--- test/controllers/moca_data_controller_test.rb
+++ test/controllers/moca_data_controller_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require 'base64'
 
 class MocaDataControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
@@ -8,9 +9,15 @@ class MocaDataControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
   
+  f = ''
   test "should post index" do
-    image = File.open("./test/fixtures/files/sample.pdf", "rb:ASCII-8BIT:UTF-8")
-    post moca_data_url, params: image
-    assert_response :success
+    # PDFファイルをPOSTで送る
+    post moca_data_url, params: { upload: file_fixture_upload('sample.pdf', 'application/pdf') }
+    # File.open("./test/fixtures/files/sample.pdf", "rb") do |file|
+    #   f = Base64.encode64(file.read)
+    # end
+    
+    # 指定のパスに遷移する
+    assert_response :redirect
   end
 end
