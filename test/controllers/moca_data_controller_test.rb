aff1dab3e8d08ebb9800628e13f424b17436ce2d
naota7118 <naota7118@gmail.com>
2024-10-19 15:57:07 +0900

    コメントを削除

--- test/controllers/moca_data_controller_test.rb
+++ test/controllers/moca_data_controller_test.rb
@@ -9,19 +9,12 @@ class MocaDataControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
   
-  f = ''
   test "should post index" do
-    # PDFファイルをPOSTで送る
-
+    # PDFファイルをPOSTリクエストで送る
     post moca_data_url, params: { upload: file_fixture_upload('sample.pdf', 'application/pdf') }
-    pdf = ''
-    # File.open("./test/fixtures/files/sample.pdf", "rb") do |file|
-    #   pdf = file.binread
-    #   post moca_data_url, params: { upload: pdf}
-    #   # f = Base64.encode64(file.read)
-    # end
 
     # 違うURLに遷移する（Google認証画面に遷移）
     assert_response :redirect
   end
+
 end
