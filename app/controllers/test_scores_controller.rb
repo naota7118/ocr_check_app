723e91194cd0140caa1eb998f782372bfb6effbd
naota7118 <naota7118@gmail.com>
2024-10-29 13:29:24 +0900

    ハッシュの形でデータを格納するためのメソッドを定義したが使わなくなったので削除した

--- app/controllers/test_scores_controller.rb
+++ app/controllers/test_scores_controller.rb
@@ -57,8 +57,6 @@ class TestScoresController < ApplicationController
     # PDFデータとExcelデータを照合
     compare(@new_pdf_scores, @excel_scores)
 
-    # ハッシュ型で1人ずつのデータを格納
-    store_in_hash
     # 照合が完了したらファイルを削除
     delete_files
     # エラーが発生したらファイルを削除
@@ -331,12 +329,6 @@ class TestScoresController < ApplicationController
       end
       @all_result << @personal_result
     end
-  end
-
-  def store_in_hash
-    @subject_ids.each do |subject|
-      
-    end  
   end
 
   # ローカルからファイルを削除する
