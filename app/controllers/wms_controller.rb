bb2c29db6e561dc521ae73cebfcea17b76f6a0be
naota7118 <naota7118@gmail.com>
2024-11-03 12:22:19 +0900

    wmsスコアを読み取るコントローラを設置

--- app/controllers/wms_controller.rb
+++ app/controllers/wms_controller.rb
@@ -1,2 +1,85 @@
+# frozen_string_literal: true
+
+require 'google/apis/drive_v3'
+require 'google/api_client/client_secrets'
+require 'roo'
+require 'rubyXL'
+require 'rubyXL/convenience_methods'
+require 'pdf/reader'
+
 class WmsController < ApplicationController
+  # ファイルアップロード用のビューを返す
+  def index; end
+
+  # フォームで送られたPDFファイルを格納しresult関数を呼び出す
+  def create
+    uploaded_file = params[:upload]
+    if uploaded_file
+      file_path = Rails.root.join("public/uploads/#{uploaded_file.original_filename}")
+      File.binwrite(file_path, uploaded_file.read)
+      redirect_to wms_result_path
+    end
+  end
+
+  # PDFとエクセルの得点データを照合し、結果を返す
+  def result
+    # Google認証
+    pass_authentication
+    return if performed?
+
+    convert_pdf_into_text(@drive)
+  end
+
+  # PDFから照合処理に必要なテキストのみ抽出（Google Drive APIのOCR技術使用）
+  def convert_pdf_into_text(drive)
+    file_path = Dir.glob(Rails.root.join('public/uploads/*.pdf').to_s)
+    # PDFファイルをGoogleドライブにアップロード
+    metadata = drive.create_file(metadata, upload_source: file_path.first, content_type: '/pdf')
+    # Googleドキュメント形式に変換
+    converted_file = drive.copy_file(metadata.id, Google::Apis::DriveV3::File.new(mime_type: 'application/vnd.google-apps.document'))
+
+    # テキストファイルを出力
+    drive.export_file(converted_file.id, 'text/plain', download_dest: './tmp/txt/wms.txt')
+    binding.pry
+    # GoogleドライブからPDFファイルを削除する
+    drive.delete_file(metadata.id)
+    # GoogleドライブからGoogleドキュメントファイルを削除する
+    drive.delete_file(converted_file.id)
+  end
+
+  private
+
+  # Google API認証を通す
+  def pass_authentication
+    # client_secret.jsonファイルを読み取ってオブジェクトを作成
+    client_secrets = Google::APIClient::ClientSecrets.load
+    auth_client = client_secrets.to_authorization
+    auth_client.update!(
+      scope: 'https://www.googleapis.com/auth/drive',
+      redirect_uri: Rails.application.credentials.dig(:google, :wms_redirect_uri),
+      additional_parameters: {
+        'access_type' => 'online',
+        'include_granted_scopes' => 'true' # incremental auth
+      }
+    )
+    if request.params['code'].nil? # 認証コードを持っていなかった場合
+      auth_uri = auth_client.authorization_uri.to_s
+      redirect_to auth_uri, allow_other_host: true
+    else # 認証コードを持っている場合
+      auth_client.code = request.params['code']
+      auth_client.fetch_access_token!
+      auth_client.client_secret = nil
+      session[:credentials] = auth_client.to_json
+
+      client_opts = JSON.parse(session[:credentials])
+      auth_client = Signet::OAuth2::Client.new(client_opts)
+      @drive = Google::Apis::DriveV3::DriveService.new.tap do |client|
+        client.client_options.open_timeout_sec = 120
+        client.client_options.read_timeout_sec = 120
+        client.request_options.retries = 3
+      end
+      @drive.authorization = auth_client
+    end
+  end
+
 end
